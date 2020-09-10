bb62f5135fd2a1d7c8f7b987f9f60cc6a04eda03
Firas <firas.mdimagh@microsoft.com>
2020-09-10 17:18:29 +0200

    Update deployment-script-lab11.sh

--- labs/lab11/deployment-script-lab11.sh
+++ labs/lab11/deployment-script-lab11.sh
@@ -13,7 +13,7 @@ echo Create Storage Account
 az storage account create -l $location -n $storageAccountName -g $resourceGroupName --access-tier hot --kind StorageV2 --sku Standard_LRS
 
 echo Get Storage Account connection string
-connectionString=$(az storage account show-connection-string -n asyncstorfiras -g AsyncProcessor --query connectionString -o tsv)
+connectionString=$(az storage account show-connection-string -n $storageAccountName -g $resourceGroupName --query connectionString -o tsv)
 
 echo Update storage-connection-string in MessageProcessor program
 search="<storage-connection-string>"
