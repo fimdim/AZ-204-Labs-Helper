15b1e321b9f48fe911a47ec92fede99fe04a99d2
Firas <firas.mdimagh@microsoft.com>
2020-09-10 15:31:11 +0200

    Update deployment-script-lab10.sh

--- labs/lab10/deployment-script-lab10.sh
+++ labs/lab10/deployment-script-lab10.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-# Parameters
+
+echo Parameters
 resourceGroupName="PubSubEvents"
 location="westeurope"
 suffix=$RANDOM
@@ -8,32 +9,43 @@ appServicePlanName="EventPlan"
 webAppNameName="eventviewer"$suffix
 subscriptionName="basicsub"
 subscriptionEndpoint="https://$webAppNameName.azurewebsites.net/api/updates"
-# Get Subscription ID
+
+echo Get Subscription ID
 subscriptionId=$(az account show --query id -o tsv)
 eventGridResourceId="/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.EventGrid/topics/$eventGridTopicName"
-# Register Microsoft.EventGrid Provider
+
+echo Register Microsoft.EventGrid Provider
 az provider register --namespace Microsoft.EventGrid
-# Create Resource Group
+
+echo Create Resource Group
 az group create -l $location -n $resourceGroupName --tags type=lab
-# Create EventGridTopic
+
+echo Create EventGridTopic
 az eventgrid topic create -l $location -n $eventGridTopicName -g $resourceGroupName
-# Create AppServicePlan
+
+echo Create AppServicePlan
 az appservice plan create -n $appServicePlanName -g $resourceGroupName -l $location --sku P1V2 --is-linux
-# Create WebApp
+
+echo Create WebApp
 az webapp create -n $location -g $resourceGroupName -p $appServicePlanName -n $webAppNameName -i "microsoftlearning/azure-event-grid-viewer:latest"
-# Create EventGrid subscription
+
+echo Create EventGrid subscription
 az eventgrid event-subscription create -n $subscriptionName --source-resource-id $eventGridResourceId --endpoint $subscriptionEndpoint --endpoint-type webhook
-# Get Topic key
+
+echo Get Topic key
 endpoint=$(az eventgrid topic show -n $eventGridTopicName -g $resourceGroupName --query endpoint -o tsv)
 key=$(az eventgrid topic key list -n $eventGridTopicName -g $resourceGroupName --query key1 -o tsv)
-# Update Topic-Endpoint in EventPublisher program
+
+echo Update Topic-Endpoint in EventPublisher program
 search="<Topic-Endpoint>"
-replace=$endpoint
+replace=$(echo $endpoint | sed 's/\//\\\//g')
 sed -i "s/${search}/${replace}/g" EventPublisher/Program.cs
-# Update Topic-Key in EventPublisher program
+
+echo Update Topic-Key in EventPublisher program
 search="<Topic-Key>"
-replace=$key
+replace=$(echo $key | sed 's/\//\\\//g')
 sed -i "s/${search}/${replace}/g" EventPublisher/Program.cs
-# If dotnet 3.1 is installed
+
+# echo If dotnet 3.1 is installed
 #cd EventPublisher
 #dotnet run
