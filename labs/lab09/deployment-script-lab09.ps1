854da0b2ae8ae24b98ab5483f10bce503a4ef05b
Houssein Ben Amor <hobenamo@microsoft.com>
2020-11-13 14:45:56 +0100

    Fix lab09

--- labs/lab09/deployment-script-lab09.ps1
+++ labs/lab09/deployment-script-lab09.ps1
@@ -1,8 +1,8 @@
 # Parameters
+$nameSuffix=Get-Random -Maximum 100000000
 $deploymentName="AZ204Lab09Deployment"
 $resourceGroupName="AutomatedWorkflow"
-$location="West Europe"
-$nameSuffix=Get-Random -Maximum 100000000
+$location="westeurope"
 $tags=@{type="lab"}
 $subscriptionId=(Get-AzSubscription).Id[0]
 
@@ -13,71 +13,73 @@ $apimAdminEmail="admin@contoso.com"
 $storageAccountName="prodstor"+$nameSuffix
 $shareName = "metadata"
 
+$apiConnectionTemplatePath=".\api-connection-template.json"
+
 $logicAppName="prodflow"+$nameSuffix
-$logicAppDefinitionFilePath=".\logic-app-definition-lab09.json"
+$logicAppDefinitionFilePath=".\logic-app-template.json"
 $connectionId="/subscriptions/"+$subscriptionId+"/resourceGroups/"+$resourceGroupName+"/providers/Microsoft.Web/connections/azurefile"
 
-# $ParametersObj = @{
-#     storageAccountName = $storageAccountName
-#     tags = $tags
-# }
+# Create Resource Group if it doesn't exist
+Get-AzResourceGroup -Name $resourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent)
+{
+   New-AzResourceGroup -Name $resourceGroupName -Location $location -Tag $tags -Force
+}
 
-# Create Resource Group
-New-AzResourceGroup -Name $resourceGroupName -Location $location -Tag $tags -Force
-
-# # Create APIM
-# New-AzApiManagement `
-#    -ResourceGroupName $resourceGroupName `
-#    -Name $apimName `
-#    -Location $location `
-#    -Organization $apimOrganization `
-#    -Sku Consumption `
-#    -AdminEmail $apimAdminEmail
-
-New-AzLogicApp `
+# Create APIM
+New-AzApiManagement `
    -ResourceGroupName $resourceGroupName `
-   -Name $logicAppName `
+   -Name $apimName `
    -Location $location `
-   -DefinitionFilePath $logicAppDefinitionFilePath `
-#    -Parameters @{connectionId=$connectionId}
+   -Organization $apimOrganization `
+   -Sku Consumption `
+   -AdminEmail $apimAdminEmail
 
-# # Create Storage Account
-# New-AzStorageAccount `
-#     -ResourceGroupName $resourceGroupName `
-#     -Name $storageAccountName `
-#     -Location $location `
-#     -SkuName Standard_LRS `
-#     -Kind StorageV2 `
-#     -AccessTier Hot `
-#     -Tag $tags
+# Create Storage Account
+New-AzStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName Standard_LRS `
+    -Kind StorageV2 `
+    -AccessTier Hot `
+    -Tag $tags
 
-# New-AzRmStorageShare `
-#     -ResourceGroupName $resourceGroupName `
-#     -StorageAccountName $storageAccountName `
-#     -Name $shareName `
-#     -QuotaGiB 1
+New-AzRmStorageShare `
+    -ResourceGroupName $resourceGroupName `
+    -StorageAccountName $storageAccountName `
+    -Name $shareName `
+    -QuotaGiB 1
 
-# # Retrieve Storage Account key
-# $storageAccountKey = (Get-AzStorageAccountKey `
-#                         -ResourceGroupName $resourceGroupName `
-#                         -AccountName $storageAccountName) `
-#                         | Where-Object {$_.KeyName -eq "key1"}
+# Retrieve Storage Account key
+$storageAccountKey = (Get-AzStorageAccountKey `
+                        -ResourceGroupName $resourceGroupName `
+                        -AccountName $storageAccountName) `
+                        | Where-Object {$_.KeyName -eq "key1"}
 
-# # Get Storage Account Context
-# $context = New-AzStorageContext `
-#             -StorageAccountName $storageAccountName `
-#             -StorageAccountKey $storageAccountKey.value
+# Get Storage Account Context
+$context = New-AzStorageContext `
+            -StorageAccountName $storageAccountName `
+            -StorageAccountKey $storageAccountKey.value
 
-# # Upload files to fileshare
-# for($i = 0; $i -lt 4; $i++)
-# {
-#     $fileSource="files/item_0"+$i+".json"
+# Upload files to fileshare
+for($i = 0; $i -lt 4; $i++)
+{
+    $fileSource="files/item_0"+$i+".json"
 
-#     Set-AzStorageFileContent `
-#     -Context $context `
-#     -ShareName $shareName `
-#     -Source $fileSource
-# }
+    Set-AzStorageFileContent `
+    -Context $context `
+    -ShareName $shareName `
+    -Source $fileSource
+}
 
+# Create API connection
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+   -TemplateFile $apiConnectionTemplatePath `
+   -StorageAccountName $storageAccountName
 
-# /subscriptions/e788effb-3869-4694-a8f6-a5f17e417bc3/resourceGroups/module9/providers/Microsoft.Web/connections/azurefile
+# Create Logic App
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -TemplateFile $logicAppDefinitionFilePath `
+    -Location $location `
+    -LogicAppName $logicAppName
